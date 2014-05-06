4049504426c8fbf749a8308f04ec0818682715e1
Vlad <k2emailbox@gmail.com>
2014-05-06 23:34:21 +0300

    some comments

---
+++ ClassPathExamples/src/PrintTextNoPack.java
@@ -0,0 +1,12 @@
+/**
+ * javac -d bin PrintTextNoPack.java
+ * cd bin
+ * java PrintTextNoPack
+ * ----------
+ * java -jar <name>.jar
+ */
+public class PrintTextNoPack {
+    public static void main(String[] args) {
+        System.out.println("One class without package");
+    }
+}
---
+++ ClassPathExamples/src/com/one/PrintTextPack.java
@@ -0,0 +1,13 @@
+package com.one;
+/**
+ * javac -d bin com/one/PrintTextPack.java
+ * cd bin
+ * java com.one.PrintTextPack
+ * ----------------
+ * java -jar <name>.jar
+ */
+public class PrintTextPack {
+    public static void main(String[] args) {
+        System.out.println("One class with package");
+    }
+}
